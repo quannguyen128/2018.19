021180395f6021ac13add08949b5118eca46a4db
Quan Nguyen <quannguyen128@gmail.com>
2018-09-19 11:09:46 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2018._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
